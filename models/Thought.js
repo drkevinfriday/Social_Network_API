35d55fb684bc4cfe6a31e3455a4d5c6e7f727a85
drkevinfriday <kevinfriday87@gmail,com>
2022-07-14 10:37:44 -0500

    model build

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model }= require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+
+// reaction Schema
+const ReactionSchema = new Schema(
+    {
+      // set custom id to avoid confusion wit parent comment _id
+    
+    ReactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    ReactionBody: {
+      type: String,
+      required: true,
+      maxlength: [281, 'Reaction text must be 280 characters or under']
+    },
+    username: {
+      type: String,
+      required: true
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+  )
+
+// thought Schema
+
+const thoughtSchema = new Schema (
+    {
+    
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: [1, 'Thought text body is to short'],
+            maxlength: [281, 'thought text must be 280 characaters or under']
+        },
+
+        createdAt: {
+            type: Date,
+            defalut: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+
+        username: {
+            type: String,
+            required: true,
+        }, 
+        // use Reaction Schema to validate for a reaction
+        reacations: [ReactionSchema]
+    },
+    {
+        toJson: {
+            getters: true
+        }
+
+    }
+)
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model }= require('mongoose');
+const dateformat = require('../utils/dateFormat');
+
+// User Schema
+
+const UserSchema = new Schema (
+    {
+        username:{
+            type: String,
+            trim: true,
+            unique: true,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: ['[a-z0-9]+@[a-z]+\.[a-z]{2,3}']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+            ],
+        freinds: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+            ]
+
+
+    }, 
+    {
+        toJSON: {
+            virtuals: true       
+        },
+ 
+    }
+)
+
+//  get total of friends count
+UserSchema.virtual('friendCount').get(function(){
+    return this.freinds.length;
+})
